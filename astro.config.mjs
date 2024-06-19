095a80c445f011f6f3eebaa9f0108004870e502c
EGARRISXN <egarrisxn@gmail.com>
2024-06-19 02:42:16 -0400

    og image, readme, and much more!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
@@ -8,7 +7,7 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
-    webAnalytics: {enabled: true},
+    webAnalytics: { enabled: true },
     imageService: true,
   }),
 });
